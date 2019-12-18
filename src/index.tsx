c3b12ea08b40045a7f46cac6d4ddf1a70ed01acc
PacersCyc <452720855@qq.com>
2019-12-18 13:33:51 +0800

    支持装饰器

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
 import * as styles from './index.scss'
+import Test from './components/Test'
 
 const render = () => {
   ReactDOM.render(
-    <div className={styles.test}>123123123123</div>,
+    // <div className={styles.test}>123123123123</div>,
+    <Test />,
     document.querySelector('#app')
   )
 }
