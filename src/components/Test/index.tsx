c3f689d9c6129bc1ef6b7f7aa6ead6bbd7d13912
PacersCyc <452720855@qq.com>
2019-12-18 14:22:11 +0800

    整合常用功能

--- src/components/Test/index.tsx
+++ src/components/Test/index.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react'
 import { Button } from 'antd'
 // import 'antd/lib/button/style/index.less'
+import { ComponentExt } from '@utils/reactExt'
 
 @log
-class Test extends React.Component {
+class Test extends ComponentExt {
+  showMsg = () => {
+    this.$message.success('这是一个通知')
+  }
+
   render() {
     return (
       <div>
-        <Button>123</Button>
+        <Button onClick={this.showMsg} type="primary">123</Button>
       </div>
     )
   }
